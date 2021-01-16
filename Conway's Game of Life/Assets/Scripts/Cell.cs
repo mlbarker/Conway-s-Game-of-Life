b6b5c6bc11334955b90acbe978fcf45b098e645c
mlbarker <marcus.barker@gmail.com>
2021-01-16 03:57:56 -0600

    Initial Checkin - Can click on cells but cell alignment is off

---
+++ Conway's Game of Life/Assets/Scripts/Cell.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Cell : MonoBehaviour
+{
+    Animator m_animator;
+
+    bool m_dead;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_animator = gameObject.GetComponent<Animator>();
+        if (m_animator == null)
+        {
+            Debug.LogError("Animator is null");
+
+            m_dead = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            m_dead = !m_dead;
+            m_animator.SetBool("CellDead", m_dead);
+
+            Debug.Log("Cell Dead : " + m_dead);
+        }
+    }
+}
---
+++ Conway's Game of Life/Assets/Scripts/Grid.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    [SerializeField]
+    private uint _gridSize = 5;
+
+    [SerializeField]
+    private GameObject _cellPrefab;
+
+    private uint _totalCells;
+    private GameObject[] _cells;
+    //private Cell[] _cells;
+
+    //[SerializeField]
+    //private GameObject[,] m_cellsGameObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _totalCells = _gridSize * _gridSize;
+        _cells = new GameObject[_totalCells];
+
+        // setup the cells' position in the grid
+        // need to do null checks here
+        float width = _cellPrefab.GetComponent<BoxCollider2D>().size.x;
+        float height = _cellPrefab.GetComponent<BoxCollider2D>().size.y;
+
+        // coordinates to set the position of each cell
+        int x = 0;
+        int y = 0;
+        for (int index = 0; index < _totalCells; ++index)
+        {
+            // instantiate the cell
+            Vector3 position = new Vector3(width * x, height * y, 1);
+            _cells[index] = Instantiate(_cellPrefab, position, Quaternion.identity);
+            
+            // update the x offset
+            x++;
+
+            // row was filled, onto the next 
+            // row (update the y offset)
+            if ((index + 1) % _gridSize == 0 && index != 0)
+            {
+                x = 0;
+                y++;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
